5d97cdb941bfbd97fbf52049e13de4ade7c9adf4
Lúcia Helena <luciahproenca@gmail.com>
2024-06-29 18:13:00 -0300

    Finalizando o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,102 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const tabelaIMC=()=>{
+  return(
+    <table border='1' style={{borderCollapse:"collapse"}}>
+      <thead>
+        <tr>
+          <th>Classificação</th>
+          <th>IMC</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Abaixo do peso</td>
+          <td>Abaixo de 18,5</td>
+        </tr>
+        <tr>
+          <td>Peso normal</td>
+          <td>Entre 18,5 e 24,9</td>
+        </tr>
+        <tr>
+          <td>Sobrepeso</td>
+          <td>Entre 25 e 29,9</td>
+        </tr>
+        <tr>
+          <td>Obesidade Grau I</td>
+          <td>Entre 30 e 34,9</td>
+        </tr>
+        <tr>
+          <td>Obesidade Grau II</td>
+          <td>Entre 35 e 39,9</td>
+        </tr>
+        <tr>
+          <td>Obesidade Grau III ou Mórbida</td>
+          <td>Maior que 40</td>
+        </tr>
+      </tbody>
+    </table>
   )
 }
 
-export default App
+const fpeso=(p,sp)=>{
+  return(
+    <div>
+      <label>
+        Peso
+        <input type="text" value={p} onChange={(e)=>{sp(e.targed.value)}}/>
+      </label>
+    </div>
+  )
+}
+
+const faltura=(a,sa)=>{
+  return(
+    <div>
+      <label>
+        Altura
+        <input type="text" value={a} onChange={(e)=>{sp(e.targed.value)}}/>
+      </label>
+    </div>
+  )
+}
+
+const fcalcular=(p,a,sr)=>{
+  const calc=()=>{
+    sr(p/(a*a))
+  }
+  return(
+    <div>
+      <button onClick={calc}>Calcular</button>
+    </div>
+  )
+  
+}
+
+const fresultado=(r)=>{
+  return(
+    <div>
+      <p>Resultado: {r.toFixed(2)}</p>
+    </div>
+  )
+}
+
+
+export default function App() {
+
+  const [peso,setPeso]=useState(0)
+  const [altura,setAltura]=useState(0)
+  const [resultado,setResultado]=useState(0)
+
+  return (
+    <>
+      {fpeso(peso,setPeso)}
+      {faltura(altura,setAltura)}
+      {fcalcular(peso,altura,setResultado)}
+      {fresultado(resultado)}
+      {tabelaIMC()}
+    </>
+    
+  )
+}
+
